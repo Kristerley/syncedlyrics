c7cbcbff1b0c22e3c43d2085378bd1fbaf5804e2
Kristerley <37936327+Kristerley@users.noreply.github.com>
2026-06-15 15:08:33 -0400

    Update regex for extracting search term from path

--- syncedlyrics/utils.py
+++ syncedlyrics/utils.py
@@ -83,7 +83,7 @@ def synced_to_plaintext(synced_lyrics: str) -> str:
     return re.sub(r"\[\d+:\d+\.\d+\] ", "", synced_lyrics)
     
 def get_search_term(search_term: str) -> str:
-    match = re.search(r"\d\.(.+)\.[flac|mp3|wav|aax]", search_term)
+     match = re.search(r"^.*[\\|\/][\d+\.]*(.+)\.[flac|mp3|wav|aax]", search_term)
     if match:
         return match.group(1)
     else:
